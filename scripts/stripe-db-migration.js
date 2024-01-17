0594ace19f1d105be568bb210dc85ab139633c3b
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-01-17 15:59:10 +0900

    Add subscriptions table

--- scripts/stripe-db-migration.js
+++ scripts/stripe-db-migration.js
@@ -21,10 +21,34 @@ async function createStripeCustomersTable(client) {
   }
 }
 
+async function createSubscriptionsTable(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`;
+
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS subscriptions (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        user_id UUID NOT NULL REFERENCES users(id),
+        stripe_subscription_id VARCHAR(255) NOT NULL,
+        stripe_invoice_id VARCHAR(255) UNIQUE,
+        start_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+        end_timestamp TIMESTAMP WITH TIME ZONE NOT NULL
+      );
+    `;
+
+    console.log(`Created "subscriptions" table`);
+    return createTable;
+  } catch (error) {
+    console.error('Error creating subscriptions table:', error);
+    throw error;
+  }
+}
+
 async function main() {
   const client = await db.connect();
 
   await createStripeCustomersTable(client);
+  await createSubscriptionsTable(client);
 
   await client.end();
 }
