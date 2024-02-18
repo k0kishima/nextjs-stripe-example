3e1d7ef21d95f0adf8ecf18db745cf56cc4ee133
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-02-18 11:44:11 +0900

    Implement budge component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,10 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import Badge from '@/app/ui/badge';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <Badge />
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
