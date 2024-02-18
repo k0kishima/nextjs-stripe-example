7133e0a3889cd3e01113f1dd70d3acc53c2db807
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-02-18 18:16:15 +0900

    Add subscription information page

---
+++ app/dashboard/subscriptions/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from '@/auth';
+import { lusitana } from '@/app/ui/fonts';
+import { getValidSubscription } from '@/app/lib/data';
+import { getCreditCard } from '@/app/lib/stripe/credit-card';
+import SubscriptionInformation from '@/app/ui/subscription-information';
+
+export default async function Page() {
+  const currentUser = await auth().then((session) => session?.user);
+  if (currentUser?.email == null) {
+    return null;
+  }
+  const subscription = await getValidSubscription(currentUser.email)
+  if (subscription == null) {
+    return null;
+  }
+  const creditCard = await getCreditCard(subscription.stripe_subscription_id)
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Subscription Information</h1>
+      </div>
+      <SubscriptionInformation subscription={subscription} creditCard={creditCard} />
+    </div>
+  );
+}
